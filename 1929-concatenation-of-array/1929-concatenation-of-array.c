3646cb5ff1f45b6e2e9d61c7820219f1908fe6f7
ArshDhole <arshdhole.cs@gmail.com>
2026-03-19 23:19:15 +0530

    Time: 0 ms (100%), Space: 15.1 MB (8.34%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.c
@@ -0,0 +1,11 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getConcatenation(int* nums, int numsSize, int* returnSize) {
+    int * arsh = (int*)malloc(2*numsSize*sizeof(int));
+    *returnSize=2*numsSize;
+    for(int i=0;i<numsSize;i++){
+        arsh[i+numsSize]=arsh[i]=nums[i];
+    }
+    return arsh;
+}
